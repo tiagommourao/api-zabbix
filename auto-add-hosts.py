c902a6eb1685699fe8103afd34b552bb70dfe47d
Tiago Mourão <tiagommourao@users.noreply.github.com>
2021-11-03 23:29:46 -0300

    Update auto-add-hosts.py

--- auto-add-hosts.py
+++ auto-add-hosts.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 
 #author: Janssen dos Reis Lima
+#ajustado para adicionar host como SNMP
+#colocado mais campos no CSV para atender a minha necessidade.
 
 from pyzabbix import ZabbixAPI
 import csv
 from progressbar import ProgressBar, Percentage, ETA, ReverseBar, RotatingMarker, Timer
 
-zapi = ZabbixAPI("http://localhost/zabbix")
-zapi.login(user="Admin", password="zabbix")
+zapi = ZabbixAPI("http://172.16.1.162")
+zapi.login(user="Admin", password="C113r#2021")
 
 arq = csv.reader(open('/tmp/hosts.csv'))
 
@@ -18,23 +20,35 @@ f = csv.reader(open('/tmp/hosts.csv'), delimiter=';')
 bar = ProgressBar(maxval=linhas,widgets=[Percentage(), ReverseBar(), ETA(), RotatingMarker(), Timer()]).start()
 i = 0
 
-for [hostname,ip] in f:
+for [hostname,ip,dns,desc] in f:
     hostcriado = zapi.host.create(
         host= hostname,
+        name= desc,
+        description= desc,
+        inventory_mode= 1,
         status= 1,
-        interfaces=[{
-            "type": 1,
+        interfaces=
+        [{
+            "type": "2",
             "main": "1",
             "useip": 1,
             "ip": ip,
-            "dns": "",
-            "port": 10050
+            "dns": dns,
+            "port": 161,
+            "details":
+                {
+                "version": 2,
+                "community": "{$SNMP_COMMUNITY}",
+                "interface_ref": "if1"
+                }
         }],
-        groups=[{
-            "groupid": 2
+        groups=
+        [{
+            "groupid": 22
         }],
-        templates=[{
-            "templateid": 10001
+        templates=
+        [{
+            "templateid": 10249
         }]
     )
 
@@ -43,4 +57,4 @@ for [hostname,ip] in f:
     bar.update(i)
 
 bar.finish
-print " "
+print (" ")
